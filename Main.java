70531fc9796f9bcf85803aba1983ea683ca6d903
abbadonref <45109082+abbadonref@users.noreply.github.com>
2018-11-17 13:29:50 +0300

    aaa

--- Main.java
+++ Main.java
@@ -1,27 +1,23 @@
 
+import java.util.Scanner;
 
-import java.util.Scanner;// импортируем класс Scanner
 
-public class Main{// объявляется класс Test
 
-    public static void main(String[] args) {// так включаются программы,
-        // просто запомнить
+public class Main{
+
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число:");
-         short a = sc.nextShort();
-        System.out.println("Введите число:");
-        short b = sc.nextShort();
-        sc.close();
-         if  ((a%2)==0)
-         {
-             System.out.println("вот сумма:" + a+b);
-         }
-         else if ((b%3)==0){
-             System.out.println("вот произведение" + a*b);
-         }
-          else {
-            System.out.println("вот нуль"+ 0);
-        }
+        double a = sc.nextShort();
+        System.out.println("enter another число");
+        double b = sc.nextShort();
+        System.out.println("число в степени a: " + Math.pow(b,a));
+
+        System.out.println("ваш корень из числа "+a+"    " + Math.sqrt(a));
 
     }
 }
+
+
+
