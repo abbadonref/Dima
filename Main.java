32f1831a232e76e7eec365f92fbb3cb75d8ad0ca
abbadonref <45109082+abbadonref@users.noreply.github.com>
2019-02-02 15:10:00 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,7 @@
-
-import java.util.Scanner;
-
-
-
-public class Main{
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число:");
-        double a = sc.nextShort();
-        System.out.println("enter another число");
-        double b = sc.nextShort();
-        System.out.println("число в степени a: " + Math.pow(b,a));
-
-        System.out.println("ваш корень из числа "+a+"    " + Math.sqrt(a));
-
-    }
+package geom;
+import geom.Point;
+import geom.Rectangle;
+public class Main {
+Point p1=new Point(10,15);
+System.out.print();
 }
-
-
-
---
+++ Point.java
@@ -0,0 +1,19 @@
+package geom;
+
+public class Point {
+    int x;
+    int y;
+    Point(){
+        x=15;
+        y=10;
+    }
+    public Point(){
+        x = 15;
+        y = 10;
+    }                                                                                                                                                                                                                                                      
+
+    public int getX(){
+        return x;
+    }
+
+}
